681c2d31df5bd369083c76e920d2f3f56c17ea15
Keri Hazelton <keri.hazelton@gmail.com>
2016-05-19 11:25:16 -0400

    add images and styling to products, add links to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,7 +14,7 @@ Rails.application.routes.draw do
    root 'static_pages#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+     get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
