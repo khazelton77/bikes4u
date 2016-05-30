e90d6465ed07fa032b05bc0282c056a66f6256a8
Keri Hazelton <keri.hazelton@gmail.com>
2016-05-30 14:59:19 -0400

    Reroute root page to about page, create dynamic links to about and contact static pages and dropdown product menu dynamically link to product page

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products 
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
 
+  get 'static_pages/landing_page'
+
   resources :orders, only: [:index, :show, :create, :destroy]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'static_pages#landing_page'
+   root 'static_pages#about'
 
   # Example of regular route:
      get 'products/:id' => 'catalog#view'
