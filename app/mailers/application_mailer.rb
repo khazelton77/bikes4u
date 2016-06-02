6d704c667e55690432be71ffe570107699f2539e
Keri Hazelton <keri.hazelton@gmail.com>
2016-06-02 11:38:36 -0400

    Set up mail function in development and production environments

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,3 +2,5 @@ class ApplicationMailer < ActionMailer::Base
   default from: "keri.hazelton@hotmail.com"
   layout 'mailer'
 end
+
+ActionMailer::Base.mail(:from => 'keri.hazelton@hotmail.com', :to => 'to@domain.com', :subject => "Welcomd to my awesome site!", :body => 'I am the email body.').deliver_now
