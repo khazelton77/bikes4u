65e9b1507a687c611d2e70dd471bbac3ca84612b
Keri Hazelton <keri.hazelton@gmail.com>
2016-06-02 14:17:41 -0400

    Correction in routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
    root 'static_pages#landing_page'
 
   # Example of regular route:
-     get 'products/:id' => 'catalog#view'
+     get 'products#:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
