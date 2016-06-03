5eb519fe121b89aa8cade37112692648ec2300bc
Keri Hazelton <keri.hazelton@gmail.com>
2016-06-03 14:12:50 -0400

    Put thank you method in proper location

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,9 +9,4 @@ class UserMailer < ApplicationMailer
 	end
 end
 
-def thank_you
-	@name = params[:name]
-	@email = params[:email]
-	@message = params[:message]
-	UserMailer.contact_form(@email, @name, @message).deliver_now
-end
+
