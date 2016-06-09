210b4fb5efc48132eb302e36458d756ed79cf055
Keri Hazelton <keri.hazelton@gmail.com>
2016-06-09 15:48:36 -0400

    Add jquery rating function and average rating div to show.html.erb

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,8 @@
 $(document).on('ready page:load', function() {
-	$('.rating').raty( {path: '/assets', scoreName: 'comment[rating]'});
-});
-	$('.rated').raty({ path: '/assets',
-		readOnly: true,
-		score: function() {
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]'} );
+	$('.rated').raty( { path: '/assets', 
+		readOnly: true, 
+		score: function(){
 			return $(this).attr('data-score');
 		}
 	});
