52fd1b87eb2ca850d71277e97de11533566c09b9
Keri Hazelton <keri.hazelton@gmail.com>
2016-06-13 15:40:39 -0400

    Styling users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,8 +10,7 @@ class UserMailer < ApplicationMailer
 
 	def welcome(user)
 		@appname = "Bikes 4 U"
-		mail( :to => user.email,
-					:subject => "Welcome to #{@appname}!")
+		mail( :to => user.email, :subject => "Welcome to #{@appname}!")
 	end
 
 end
