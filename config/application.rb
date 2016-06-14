2f6d8ee836ff451961fae4aa9a270dc407b6f672
Keri Hazelton <keri.hazelton@gmail.com>
2016-06-14 06:20:35 -0400

    Change configuration to make user registration feature work in the development environment

--- config/application.rb
+++ config/application.rb
@@ -32,4 +32,6 @@ module Bikes4u
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
+ActionMailer::Base.deliver_method = :smtp
+
 end
