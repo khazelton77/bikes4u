493af0271525322e0abde7374619b28f810bdbe4
Keri Hazelton <keri.hazelton@gmail.com>
2016-06-21 14:25:00 -0400

    Use RSpec to test Users controller

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+  context "GET #show" do
+    before do
+      sign_in user
+
+    it "Loads correct user details"
+      get :show, id: current_user.id
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+      expect(assigns(:user)).to eq user
+    end
+
+  end
+
+  let(:user) { User.create!(email: "chaotic177@hotmail.com", password: "123456")}
+
+  describe "GET #show" do
+    context "User is logged in" do
+
+    end
+
+    context "No user is logged in" do
+      it "redirects to login" do
+        get :show, id: user.id
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+
+end
