e9b27a607e975bf2939697b566ad92b915c1867d
Keri Hazelton <keri.hazelton@gmail.com>
2016-06-24 16:52:06 -0400

    Add js comment functions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,14 +7,15 @@ class CommentsController < ApplicationController
 			if @comment.save
 				format.html { redirect_to @product, notice: 'Review was created successfully.' }
 				format.json { render :show, status: :created, location: @product }
-			else 
+				format.js
+			else
 				format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
 				format.json { render json: @comment.errors, status: :unprocessable_entity }
 			end
 		end
 	end
 
-	
+
 
 	def destroy
 		@comment = Comment.find(params[:id])
@@ -25,7 +26,7 @@ class CommentsController < ApplicationController
 
 
 private
-	
+
 	def comment_params
 		params.require(:comment).permit(:user_id, :body, :rating)
 	end
