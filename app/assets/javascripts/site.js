6dab562af1b757b3250503b9dcfa9d6f808b96ee
Keri Hazelton <keri.hazelton@gmail.com>
2016-06-24 15:57:03 -0400

    Add elevateZoom plugin to product page

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,12 @@
 $(document).on('ready page:load', function() {
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]'} );
-	$('.rated').raty( { path: '/assets', 
-		readOnly: true, 
+	$('.rated').raty( { path: '/assets',
+		readOnly: true,
 		score: function(){
 			return $(this).attr('data-score');
 		}
 	});
+
+$('.img-zoom').elevateZoom();
+
 });
